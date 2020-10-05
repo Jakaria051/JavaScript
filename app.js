b830b27bf57b22ea524b7d37419a1be4a99d4cf5
Jakaria051 <jacjakaria51@gmail.com>
2020-10-06 00:02:51 +0600

    quey selector

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 var titles = document.getElementsByClassName('title');
-console.log(Array.isArray(titles)); //this willl return false 
-console.log(Array.isArray(Array.from(titles)));  //this will turn into an array & will return true
-
-
-
+//console.log(Array.isArray(titles)); //this willl return false 
+//console.log(Array.isArray(Array.from(titles)));  //this will turn into an array & will return true
 
 Array.from(titles).forEach(function(item) {
-    console.log(item);
+   // console.log(item);
+});
+
+
+///Query Selector
+const singleItem = document.querySelector('#book-list li:nth-child(2) .name');
+//console.log(singleItem);
+const allItems = document.querySelectorAll('#book-list li .name');
+//console.log(allItems);
+
+Array.from(allItems).forEach(function(singleItem){
+console.log(singleItem);
 });
