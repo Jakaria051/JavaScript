256521de912e8c7eef465014cc1ee2d79a59b082
Jakaria051 <jacjakaria51@gmail.com>
2020-10-18 11:09:49 +0600

    chage element

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
-var titles = document.getElementsByClassName('title');
-//console.log(Array.isArray(titles)); //this willl return false 
-//console.log(Array.isArray(Array.from(titles)));  //this will turn into an array & will return true
+var allbooks = document.querySelectorAll("#book-list li .name");
 
-Array.from(titles).forEach(function(item) {
-   // console.log(item);
+Array.from(allbooks).forEach(function(singlebook){
+   singlebook.textContent += '(single book title)';
 });
 
-
-///Query Selector
-const singleItem = document.querySelector('#book-list li:nth-child(2) .name');
-//console.log(singleItem);
-const allItems = document.querySelectorAll('#book-list li .name');
-//console.log(allItems);
-
-Array.from(allItems).forEach(function(singleItem){
-console.log(singleItem);
-});
+const bookList = document.querySelector('#book-list');
+//bookList.innerHTML = '<h2>H2 Heading will be added & replace the whole thing...</h2>';
+bookList.innerHTML += '<p>This will add after other text</p>';
