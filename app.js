af5a28b4164aa9a21b068231759c5551b317d7e4
Jakaria051 <jacjakaria51@gmail.com>
2020-10-18 11:48:47 +0600

    node & traversing is added

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-var allbooks = document.querySelectorAll("#book-list li .name");
-
-Array.from(allbooks).forEach(function(singlebook){
-   singlebook.textContent += '(single book title)';
-});
+// const banner = document.querySelector('#page-banner');
+// console.log("banner node type",banner.nodeType);
+// console.log("banner node name",banner.nodeName);
+// const clonedBanner = banner.cloneNode(true);
+// console.log(clonedBanner);
 
 const bookList = document.querySelector('#book-list');
-//bookList.innerHTML = '<h2>H2 Heading will be added & replace the whole thing...</h2>';
-bookList.innerHTML += '<p>This will add after other text</p>';
+console.log('The parent node is', bookList.parentNode);
+console.log('The Parent element is',bookList.parentElement);
+console.log('The Parent element is',bookList.parentElement.parentElement);
+console.log('the children is',bookList.children);
+
+//sibling
+console.log('book-list next  sibling',bookList.nextSibling);
+console.log('book-list next element sibling',bookList.nextElementSibling);
+
+console.log('book-list previous  sibling',bookList.previousSibling);
+console.log('book-list previous element sibling',bookList.previousElementSibling);
+
+///
+bookList.previousElementSibling.querySelector('p').innerHTML += '<br/> cool way to add new text';
