2254710690078377cd615f43151d6c88532b6722
Jakaria051 <jacjakaria51@gmail.com>
2020-10-20 12:33:51 +0600

    forms & addd element-class-append

--- app.js
+++ app.js
@@ -1,30 +1,40 @@
-// var h2 = document.querySelector('#book-list h2')
-// h2.addEventListener('click',function(e){
-//     console.log(e.target);
-//     console.log(e);
-// })
-
-// Event 
-// var btns = document.querySelectorAll('#book-list .delete');
-// Array.from(btns).forEach(function(btn){
-//     btn.addEventListener('click',function(e){
-//         const li = e.target.parentElement;
-//         li.parentNode.removeChild(li)
-//     });
-// });
-
-const link = document.querySelector('#page-banner a');
-link.addEventListener('click',function(e){
-    e.preventDefault();
-    console.log('Navigation to',e.target.textContent,'was prevented');
-});
-
-///Event Bubling efficient way...
 
 const list = document.querySelector('#book-list ul');
+
 list.addEventListener('click',function(e){
-    if(e.target.className == 'delete') {
+    if(e.target.className == 'delete'){
         const li = e.target.parentElement;
         list.removeChild(li);
     }
+});
+
+///form dynamic submit 
+
+const addForm = document.forms['add-book'];
+
+
+addForm.addEventListener('submit',function(e){
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]').value;
+  //  console.log(value);
+
+
+///Create element
+const li = document.createElement('li');
+const bookName = document.createElement('span');
+const deleteBtn = document.createElement('span');
+
+//add content
+deleteBtn.textContent = 'delete';
+bookName.textContent = value;
+
+///add claasses
+bookName.classList.add('name');
+deleteBtn.classList.add('delete');
+
+//append to document
+li.appendChild(bookName);
+li.appendChild(deleteBtn);
+list.appendChild(li);
+
 });
