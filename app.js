8d9b685f5162942607b8241542b6d87f62191dec
jacjakaria051 <jacjakaria51@gmail.com>
2022-02-07 16:03:40 +0600

    distructruing

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
-var numbers = [1,2];
-var numbers2 = [7,8];
-//traditional way
-// var newNumbers = [numbers[0],numbers[1],3,4]; 
-//spread opeartor
- var newNumbers = [...numbers,3,4]; 
- var newNumbers2 = [...numbers,...numbers2]; //it will work as like concat 
 
- var immutable = [...newNumbers]; //exact copy of number which is not going to change if newNumbers array added something new 
- newNumbers.push(5);
- console.log(immutable);
-console.log(newNumbers);
-
-///REST OPERATOR ---when user given unlimited parameter that time we use this
-
-function myFunc(...params) {
-    console.log(params);
+//Object distructuring
+const student = {
+    id: 10,
+    name: "Jack",
+    dept: "CSE",
+    age: "25",
+    education: {
+        degree: "bechelor",
+        year: "2019"
+    }
 }
 
-myFunc(10,202,30,40,50,60,70);
+//take name from student-object & asssign it to another variable
+// const { name: studentName } = student;
+// console.log(studentName);
+
+///NESTED destructing
+
+const {education: { degree: x } = {} } = student;
+console.log(x);
+
+//Array distructuring
+
+var numbers = [1,2,3,4,5,6,7];
+var [,a,,,b] = numbers;
+console.log(a,b);
+
+var arrayNested = [1,2,3,[10,20,30,40],4,5,6,7];
+var [,,,[,a,b]] = arrayNested;
+console.log(a,b);
+
+//swap the value
+var a = 1;
+var b = 2;
+[b,a] = [a,b]; 
+console.log(a,b);
