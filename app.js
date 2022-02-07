2ce1a1f825faf435c97c9c8329438cb521543713
jacjakaria051 <jacjakaria51@gmail.com>
2022-02-07 15:22:18 +0600

    spread & rest operator

--- app.js
+++ app.js
@@ -1,30 +1,20 @@
-var numbers = [1,2,3,4,5,6];
+var numbers = [1,2];
+var numbers2 = [7,8];
+//traditional way
+// var newNumbers = [numbers[0],numbers[1],3,4]; 
+//spread opeartor
+ var newNumbers = [...numbers,3,4]; 
+ var newNumbers2 = [...numbers,...numbers2]; //it will work as like concat 
 
-//Array.prototype.map()
+ var immutable = [...newNumbers]; //exact copy of number which is not going to change if newNumbers array added something new 
+ newNumbers.push(5);
+ console.log(immutable);
+console.log(newNumbers);
 
-var result = numbers.map((num) => {
-    return 2* num;
-});
-console.log(result);
-//Array.prototype.reduce()
+///REST OPERATOR ---when user given unlimited parameter that time we use this
 
+function myFunc(...params) {
+    console.log(params);
+}
 
-var sum = numbers.reduce((previousValue,currentValue) => {
-    return previousValue + currentValue;
-},0);
-
-console.log(sum);
-
-//Object
-var myObj = {
-    name: "Javascript",
-    founder: "Brenden Eich",
-    estd: "1995",
-    ranking: 1
-};
-
-var keys = Object.keys(myObj);
-var values = Object.values(myObj);
-var entries = Object.entries(myObj);
-
-console.log(entries);
+myFunc(10,202,30,40,50,60,70);
