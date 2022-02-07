bfb211e561159b19732f0567ec55e72f40276eba
jacjakaria051 <jacjakaria51@gmail.com>
2022-02-07 19:16:44 +0600

    tamplate literals

--- app.js
+++ app.js
@@ -1,37 +1,11 @@
-
-//Object distructuring
-const student = {
-    id: 10,
-    name: "Jack",
-    dept: "CSE",
-    age: "25",
-    education: {
-        degree: "bechelor",
-        year: "2019"
-    }
+function modifier(strings,...values) {
+    const a = strings.reduce((prev,current) => {
+        return prev + current + (values.length ? "Mr. " + values.shift() : "");
+    },"");
+    return a;
 }
 
-//take name from student-object & asssign it to another variable
-// const { name: studentName } = student;
-// console.log(studentName);
+var player1 = "sakib";
+var player2 = "mashrafee";
 
-///NESTED destructing
-
-const {education: { degree: x } = {} } = student;
-console.log(x);
-
-//Array distructuring
-
-var numbers = [1,2,3,4,5,6,7];
-var [,a,,,b] = numbers;
-console.log(a,b);
-
-var arrayNested = [1,2,3,[10,20,30,40],4,5,6,7];
-var [,,,[,a,b]] = arrayNested;
-console.log(a,b);
-
-//swap the value
-var a = 1;
-var b = 2;
-[b,a] = [a,b]; 
-console.log(a,b);
+console.log(modifier`We have ${player1} and ${player2} in our cricket team`);
